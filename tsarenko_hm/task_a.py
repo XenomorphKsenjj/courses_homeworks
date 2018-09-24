3e0fe6687c6b4e0f12ba4f7a9fa9d691f94d0573
Oksana Tsarenko <ksenj11@gmail.com>
2018-09-24 22:42:36 +0300

    [ADD]Edited HM Tsarenko

--- tsarenko_hm/task_a.py
+++ tsarenko_hm/task_a.py
@@ -1,15 +1,10 @@
 import random
 
-number = int(input ("Please enter a number:"))
+number = int(input("Please enter a number:"))
 
-random.number = int(random.randint(0,number))
+random_number = (random.randint(0, number))
 
-print("The random number from 0 to", number, "is", random.number)
+print("The random number from 0 to", number, "is", random_number)
 
-print("Here are", random.number, "numbers from range 0 to", number, ":", random.sample(range(number), random.number))
-
-
-
-
-
-
+print("Here are", random_number, "numbers from range 0 to", number, ":",
+      random.sample(range(number), random_number))
--- tsarenko_hm/task_b.py
+++ tsarenko_hm/task_b.py
@@ -1,13 +1,7 @@
 import random
 
-random.number = int(random.randint(1000, 1000000))
+random_number = (random.randint(1000, 1000000))
 
-result.4 = int((random.number%10000)-(random.number%1000))//1000
+result_4sign = ((random_number % 10000) - (random_number % 1000)) // 1000
 
-print("The random number is", random.number, "its 4th sign is", result.4)
-
-
-
-
-
-
+print("The random number is", random_number, "its 4th sign is", result_4sign)
